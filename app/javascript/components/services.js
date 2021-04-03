f787de3586904caf29f846379d50cd3d9395cacf
stephanie rousset <strabon@club-internet.fr>
2021-04-03 14:55:35 +0200

    add ternary to pluralize service in office show

--- app/javascript/components/services.js
+++ app/javascript/components/services.js
@@ -1,5 +1,6 @@
 
 const services = () => {
+
   const serviceField = document.querySelector('.services-fields');
   serviceField.addEventListener('change', function(){
     const services = document.querySelectorAll('.service');
@@ -16,15 +17,15 @@ const services = () => {
           let text;
           first.querySelectorAll('option').forEach(option => {
             if (option.value === first.value) {
-              text = option.innerText;
+              text = second.value <= 1 ? option.innerText : (option.innerText + 's')
             }
           });
-          serviceInfo.insertAdjacentHTML('afterend', ` <span id=${index}>${second.value} ${text}(s)</span> `);
+          serviceInfo.insertAdjacentHTML('afterend', `<span id=${index}> ${second.value} ${text}</span>` );
         }
       })
     });
   })
-}
+};
 
 
 export { services }
